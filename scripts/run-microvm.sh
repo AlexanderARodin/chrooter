00bf5aa0c38a291f8f9e7c78e4eb70408a65d0c1
a name <no@e.mail>
2025-04-20 15:13:11 +0300

    saving

--- scripts/run-microvm.sh
+++ scripts/run-microvm.sh
@@ -3,5 +3,14 @@
 echo 'started ..'
 
 qemu-system-x86_64-microvm \
-	test.img
+	-machine accel=kvm \
+	-bios /usr/share/qemu/qboot.rom \
+	-m 512M \
+	-kernel target/vmlinuz64 \
+	-initrd target/corepure64.gz \
+	-append "earlyprintk=ttyS0 console=ttyS0 showapps text" \
+	-enable-kvm -smp 2 -nographic \
+	>> target/qemu_raa.log
+
+echo '.. ended'
 
